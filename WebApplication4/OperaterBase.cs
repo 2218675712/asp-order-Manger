52430227b16582f77d0b365f75077614d732e907
2218675712 <2218675712@qq.com>
2020-10-16 14:52:46 +0800

    2020年10月16日 14:51:35 订单管理系统初始化 配置数据库 配置操作数据库的方法

---
+++ WebApplication4/OperaterBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication4
+{
+    public class OperaterBase
+    {
+        /// <summary>
+        /// 查询数据库
+        /// </summary>
+        /// <param name="sql">sql语句</param>
+        /// <returns>返回dataset数据</returns>
+        public static DataSet GetData(string sql)
+        {
+            string conn = ConfigurationManager.ConnectionStrings["orderManager"].ConnectionString;
+            SqlConnection sct = new SqlConnection(conn);
+            SqlDataAdapter sda = new SqlDataAdapter(sql, sct);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            return ds;
+        }
+
+        /// <summary>
+        /// 增删改
+        /// </summary>
+        /// <param name="sql">sql语句</param>
+        /// <returns>返回修改行数</returns>
+        public static int CommandBySql(string sql)
+        {
+            string conn = ConfigurationManager.ConnectionStrings["orderManager"].ConnectionString;
+            SqlConnection sct = new SqlConnection(conn);
+            SqlCommand smd = new SqlCommand(sql, sct);
+            sct.Open(); //打开数据库链接
+            int flag = smd.ExecuteNonQuery();
+            sct.Close(); //关闭数据库链接
+            return flag;
+        }
+    }
+}
