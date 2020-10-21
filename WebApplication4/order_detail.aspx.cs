8ab0fa6730192800dd3e05178bb3deaabf581fad
2218675712 <2218675712@qq.com>
2020-10-21 13:25:20 +0800

    开始用户添加订单页面 设备编号下拉框完成

--- WebApplication4/order_detail.aspx.cs
+++ WebApplication4/order_detail.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,35 @@ namespace WebApplication4
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                GetDeviceList();
+            }
+        }
 
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// 获取设备型号
+        /// </summary>
+        private void GetDeviceList()
+        {
+            DataSet ds = OperaterBase.GetData("select * from Device_List where is_delete=0");
+            DropDownList1.DataSource = ds;
+            DropDownList1.DataTextField = "device_number";
+            DropDownList1.DataValueField = "id";
+            DropDownList1.DataBind();
+        }
+
+        /// <summary>
+        /// 点击下拉列表
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
         }
     }
 }
