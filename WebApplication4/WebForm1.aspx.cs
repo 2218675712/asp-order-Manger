54f89cf73b426140dd367c98989871652cfe91b5
xaotag <3036905349@qq.com>
2020-11-10 08:55:03 +0800

    变更前提交

---
+++ WebApplication4/WebForm1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Label1.Text = string.Compare("abc", "ABC", true).ToString();
+            //  DateTime de = DateTime.Now;
+            //   Label2.Text = String.Format("{0:D}", de);
+
+            int[] arr = new int[10];
+            for (var i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i;
+                Response.Write("conles.log(第" + i + "个是i)");
+            }
+        }
+    }
+}
